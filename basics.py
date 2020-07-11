cdcef65f4e53079180e73d441bd3ca5e388f0d5e
sujalgera01 <sujalgera01@gmail.com>
2020-07-11 21:47:49 +0530

    basics

---
+++ basics.py
@@ -0,0 +1,24 @@
+# Python Basics
+''' Multiline Comments '''
+
+# Basic Calculations
+a = 5
+b = 10
+c = a/b
+print("a/b = ",c)
+
+# User Input
+d = int(input("Enter the value of d: "))
+print(d)
+
+# Use of if-else statement
+if d%2 == 0:
+    print("Number is Even: ",d)
+else:
+    print("Number is Odd: ",d)
+
+# tables (for loops)
+x = int(input("Enter the number: "))
+for i in range(1,11):
+    print(i*x)
+
