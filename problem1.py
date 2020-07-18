c014581f76de449db23d4f01fb9ea57a98cd1ec8
sujalgera01 <sujalgera01@gmail.com>
2020-07-18 21:12:57 +0530

    change

--- problem1.py
+++ problem1.py
@@ -7,13 +7,13 @@
 
 
 n = int(input("enter the number"))
-if n>=1 & n<=100:
+if n>=1 and n<=100:
     if n%2 != 0:
         print("Weird")
     else:
-        if n>=2 & n<6:
+        if n>=2 and n<6:
             print("Not Weird")
-        elif (n>=6 & n<21):
+        elif (n>=6 and n<21):
             print("Weird")
         else:
             print("Not Weird")
